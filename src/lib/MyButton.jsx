872483542c191bd34d70105e0fc1e555b3528091
Lívia Almeida <liviaab7@gmail.com>
2020-07-21 11:01:48 -0300

    Add color and onClick props to MyButton

--- src/lib/MyButton.jsx
+++ src/lib/MyButton.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Button from '@material-ui/core/Button'
 
-const MyButton = () =>  (
-  <Button variant="contained" color="primary">
-    Hello Livia
-  </Button>
-)
+const MyButton = ({ color, onClick }) =>  {
+  const _color = color ? color : "primary"
+
+  return (
+    <Button variant="contained" color={_color} onClick={onClick} >
+      Hello Livia
+    </Button>
+  )
+}
 
 export default MyButton
